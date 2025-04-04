f3d22fefde304f9e3a5d7cfb2a4007cdc6e81cbd
Алексей <ix.fenix@mail.ru>
2025-04-04 13:58:57 +0300

    init: project setup

---
+++ LudumDare57/Assets/_Source/GameAssembly/Core/GameInstaller.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Core
+{
+    public class GameInstaller : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
