d5f1fd3e45c03b07921557b831c578286d0cf888
Алексей <ix.fenix@mail.ru>
2025-04-06 22:17:43 +0300

    feat: level control & presets

---
+++ LudumDare57/Assets/_Source/GameAssembly/Levels/LevelControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Levels
+{
+    public class LevelControl : MonoBehaviour
+    {
+        [SerializeField] private List<int> allowGameScenesIndexes = new();
+        [SerializeField] private int shopSceneIndex;
+
+        private void Start() => DontDestroyOnLoad(gameObject);
+
+        public void LoadNextRandomLevel()
+        {
+            var nextSceneIndex =
+                allowGameScenesIndexes.Except(new[] { SceneManager.GetActiveScene().buildIndex }).ToList()[
+                    Random.Range(0, allowGameScenesIndexes.Count)];
+
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+
+        public void LoadShopLevel() => SceneManager.LoadScene(shopSceneIndex);
+    }
+}
